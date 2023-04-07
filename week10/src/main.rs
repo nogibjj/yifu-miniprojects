a0d0ad627eb3c69c23b753f51882ce8324720acf
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-04-07 18:54:39 +0000

    calculator

--- week10/src/main.rs
+++ week10/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    let mut operator = String::new();
+
+    println!("Enter first number: ");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Failed to read line");
+    let num1: f64 = num1.trim().parse().expect("Invalid input");
+
+    println!("Enter operator (+, -, *, /): ");
+    io::stdin()
+        .read_line(&mut operator)
+        .expect("Failed to read line");
+
+    println!("Enter second number: ");
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Failed to read line");
+    let num2: f64 = num2.trim().parse().expect("Invalid input");
+
+    let result = match operator.trim() {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => num1 / num2,
+        _ => {
+            println!("Invalid operator");
+            return;
+        }
+    };
+
+    println!("Result: {}", result);
 }
